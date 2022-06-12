a64251d96308a85a0bc918374ec480542d0c8110
user <Acolina711@gmail.com>
2022-06-12 17:18:34 -0300

    Tarea

--- M3U3/routes/index.js
+++ M3U3/routes/index.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
+var novedadesModel = require('../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.splice(0, 5);
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.url(novedad.img_id, {
+        width: 460,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: '/images/noimagen.jpg'
+      }
+    }
+  });
+
+  res.render('index', {
+    novedades
+  });
 });
 
 
@@ -31,8 +57,8 @@ router.post('/', async (req, res, next) => {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS
     }
-  })
-  
+  });
+
 
   var info = await transporter.sendMail(obj);
 
